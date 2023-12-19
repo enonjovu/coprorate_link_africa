dc261cb5a4d8acf037a5d501866f622ab2eb02f7
astro-jets <mphatsomtogolo30@gmail.com>
2023-12-19 15:12:06 +0200

    Dealing with self is undefined error

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,5 +1,4 @@
 import LatestNews from './components/LatestNews'
-import Ads from './components/Advertisment'
 import Hero from './components/Hero'
 import Videos from './components/Videos'
 
@@ -7,17 +6,8 @@ import Videos from './components/Videos'
 export default async function Home() {
   return (
       <main id="content">
-
-          {/* hero big grid */}
           <Hero/>
-
-          {/* advertisement */}
-          {/* <Ads/> */}
-
           <LatestNews/>
-
-
-          {/* Videos*/}
           <Videos/>
       </main>
   )
