ed3e25e94b1ca1759530dc8290a88550c2db2b8f
astro-jets <mphatsomtogolo30@gmail.com>
2024-01-04 10:02:44 +0200

    Edited contacts page3

--- app/(client)/contacts/page.tsx
+++ app/(client)/contacts/page.tsx
@@ -1,5 +1,7 @@
-'use client'
+"use client"
+
 import MapComponent from "../components/MapComponent";
+
 const Contacts = () => {
   return ( 
         <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
