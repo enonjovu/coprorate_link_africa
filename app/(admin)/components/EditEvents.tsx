226c900d88f5c1387e576e15c44b6f8b6fa1acdd
astro-jets <mphatsomtogolo30@gmail.com>
2024-01-21 23:15:46 +0200

    Worked with events

--- app/(admin)/components/EditEvents.tsx
+++ app/(admin)/components/EditEvents.tsx
@@ -1,13 +1,11 @@
 "use client"
 import "@uploadthing/react/styles.css";
-import { BsTrash } from "react-icons/bs";
+import { BsCalendar, BsClock, BsPencil } from "react-icons/bs";
 import Modal from "@/app/(admin)/components/Modal";
-import { deleteImage, postBlog, updateBlog } from "@/app/action";
-import { UploadButton } from "@/utils/uploadthing";
+import { updateEvent } from "@/app/action";
 import { useEffect, useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
 
 type singleEventProps = {
@@ -15,7 +13,10 @@ type singleEventProps = {
     title: string,
     description: string,
     images: { key: string, url: string }[],
-    date: string
+    date: string,
+    time: string,
+    venue: string,
+    eventDate: string,
 }[]
 
 // Initial state with types
@@ -23,12 +24,18 @@ type FormData = {
     id: string,
     title: string,
     description: string,
+    time: string,
+    venue: string,
+    eventDate: string,
     images: { key: string, url: string }[],
 };
 
 const initialFormData: FormData = {
     id: "",
     title: "",
+    time: "",
+    venue: "",
+    eventDate: "",
     description: "",
     images: [{ url: "", key: "" }],
 };
@@ -38,8 +45,6 @@ const EditEventComponent = ({ event }: { event: singleEventProps }) => {
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const [formKey, setFormKey] = useState<number>(0);
     const [formData, setFormData] = useState<FormData>(initialFormData);
-    const [images, setImages] = useState<{ url: string; key: string; }[]>([])
-    const [loading, setLoading] = useState(false);
     const [modalMessage, setModalMessage] = useState("");
     const router = useRouter();
 
@@ -51,37 +56,12 @@ const EditEventComponent = ({ event }: { event: singleEventProps }) => {
     }, [])
 
     const handleSubmit = async () => {
-        if (images) {
-            images.map(image => (
-                formData.images.push(image)
-            ));
-        }
-        const res = await updateBlog(formData, formData.id);
+        const res = await updateEvent(formData, formData.id);
         setModalMessage(res.message)
         setIsModalOpen(true);
         console.log("file", formData);
+        router.refresh()
     };
-
-    const handleDelete = async (key: string, id: string) => {
-        setLoading(true);
-        const res = await deleteImage(key, id);
-        if (res) {
-            // Remove the deleted image from formData
-            setFormData(prevState => ({
-                ...prevState,
-                images: prevState.images.filter(image => image.key !== key)
-            }));
-            setLoading(false);
-            setModalMessage("Image Deleted successfully")
-            setIsModalOpen(true);
-        }
-        router.push(`/admin/news/edit/${formData.id}`);
-    };
-
-    const handleImagesUpload = (res: any) => {
-        setImages(res)
-        alert("Upload Completed");
-    }
 
     return (
         <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
@@ -93,41 +73,11 @@ const EditEventComponent = ({ event }: { event: singleEventProps }) => {
                 </div>
                 <Modal isOpen={isModalOpen} message={modalMessage} onClose={() => setIsModalOpen(false)} />
                 <div className="mt-12 sapce-x-6">
-                    <div className="flex flex-row flex-wrap -mx-3">
-                        {formData.images?.map(image => (
-                            <div key={image.key} className="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-                                <div className="flex flex-row sm:block hover-img relative">
-                                    <div className="w-full flex justify-end absolute h-7">
-                                        {
-                                            loading ?
-                                                <button
-                                                    className="w-2/5 text-white flex justify-center items-center bg-red-700 rounded-sm cursor-pointer"
-                                                >
-                                                    Deleting...
-                                                </button>
-                                                :
-                                                <button
-                                                    className="w-1/5 flex justify-center items-center bg-red-700 rounded-sm cursor-pointer"
-                                                    onClick={() => handleDelete(image.key, formData.id)}
-                                                >
-                                                    <BsTrash color={"#fff"} size={20} />
-                                                </button>
-                                        }
-                                    </div>
-                                    <Image width={900} height={800} priority
-                                        className="max-w-full w-full h-40 min-h-40 max-h-40 overflow-hidden object-cover mx-auto"
-                                        src={image.url} alt="alt title"
-                                    />
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-
                     {/* Form */}
                     <form key={formKey}>
                         <div className="grid gap-4 lg:gap-6">
                             <div>
-                                <label htmlFor="title" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">Article Title</label>
+                                <label htmlFor="title" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">Event Title</label>
                                 <input type="text" name="title" id="title" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
                                     onChange={(e) => {
                                         setFormData({
@@ -139,30 +89,52 @@ const EditEventComponent = ({ event }: { event: singleEventProps }) => {
                                 />
                             </div>
 
-                            {/* Grid */}
-                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+                            <div className="flex justify-between sm:grid-cols-2 gap-4 lg:gap-6">
+                                {/* Time */}
                                 <div>
-                                    <label htmlFor="blog-image" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">Add New Images</label>
-                                    <UploadButton
-                                        endpoint="imageUploader"
-                                        onClientUploadComplete={(res) => {
-                                            if (res) {
-                                                // Do something with the response
-                                                handleImagesUpload(res);
-                                            }
+                                    <label htmlFor="time" className="flex space-x-2 mb-2 text-sm text-gray-700 font-medium dark:text-white"><BsClock size={20} color={"#fff"} /> <p>Time</p></label>
+                                    <input type="text" name="time" id="time" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                                        onChange={(e) => {
+                                            setFormData({
+                                                ...formData,
+                                                time: e.target.value.toLocaleLowerCase()
+                                            })
                                         }}
-                                        onUploadError={(error: Error) => {
-                                            // Do something with the error.
-                                            alert(`ERROR! ${error.message}`);
+                                        value={formData.time}
+                                    />
+                                </div>
+                                {/* eventDate */}
+                                <div>
+                                    <label htmlFor="eventDate" className="flex space-x-2 mb-2 text-sm text-gray-700 font-medium dark:text-white"><BsCalendar size={20} color={"#fff"} /> <p>eventDate</p></label>
+                                    <input type="text" name="eventDate" id="eventDate" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                                        onChange={(e) => {
+                                            setFormData({
+                                                ...formData,
+                                                eventDate: e.target.value
+                                            })
                                         }}
+                                        value={formData.eventDate}
+                                    />
+                                </div>
+
+                                {/* Venue */}
+                                <div>
+                                    <label htmlFor="venue" className="flex space-x-2 mb-2 text-sm text-gray-700 font-medium dark:text-white"><FaMapMarkerAlt size={20} color={"#fff"} /> <p>Location</p></label>
+                                    <input type="text" name="venue" id="venue" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                                        onChange={(e) => {
+                                            setFormData({
+                                                ...formData,
+                                                venue: e.target.value
+                                            })
+                                        }}
+                                        value={formData.venue}
                                     />
                                 </div>
                             </div>
-                            {/* End Grid */}
 
                             <div>
-                                <label htmlFor="description" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">description</label>
-                                <textarea id="description" wrap="hard" name="description" rows={20} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                                <label htmlFor="description" className="block mb-2 text-sm text-gray-700 font-medium dark:text-white">Event Description</label>
+                                <textarea id="description" wrap="hard" name="description" rows={10} className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
                                     onChange={(e) => {
                                         setFormData({
                                             ...formData,
@@ -178,8 +150,10 @@ const EditEventComponent = ({ event }: { event: singleEventProps }) => {
 
                         <div className="mt-6 grid">
                             <button type="button"
-                                className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                onClick={handleSubmit}>Update Event
+                                className="w-full space-x-3 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                                onClick={handleSubmit}>
+                                <p>Edit Event</p>
+                                <BsPencil size={20} color={"#fff"} />
                             </button>
                         </div>
                     </form>
--- app/action.ts
+++ app/action.ts
@@ -198,6 +198,16 @@ export const postEvent = async (formData:{}) : Promise<{status:string,message:st
   if(!response.ok){throw new Error("Error Posting Data")}
   return await response.json();
 }
+export const updateEvent = async (formData:{},id:string) : Promise<{status:string,message:string}> =>{
+  const response = await fetch(`${rootLink}/api/events/edit/?id=${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      cache: "no-store",
+      body: JSON.stringify(formData),
+  });
+  if(!response.ok){throw new Error("Error Posting Data")}
+  return await response.json();
+}
 export const deleteEvent = async (id:string) : Promise<{status:string,message:string}> =>{
   const response = await fetch(`${rootLink}/api/events/delete/?id=${id}`,{method:"GET",next:{revalidate:0}});
   if(!response.ok){throw new Error("Error Fetching Data")}
