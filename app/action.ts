74d242e9f439af6c31167cb678606357f94e9a63
astro-jets <mphatsomtogolo30@gmail.com>
2024-03-08 12:22:22 +0200

    changed tenders page

--- app/action.ts
+++ app/action.ts
@@ -597,38 +597,3 @@ export const searchStories = async (
   }
   return await response.json();
 };
-
-export const signUp = async (formData: {}): Promise<
-  | {
-      message: string;
-      status: string;
-    }
-  | any
-> => {
-  try {
-    const response = await fetch(`${process.env.ROOT_LINK}/api/signup`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      cache: "no-store",
-      body: JSON.stringify(formData),
-    });
-    const res = await response.json();
-    return res;
-  } catch (e) {
-    console.error(e);
-  }
-};
-
-export const signIn = async (formData: {}): Promise<any> => {
-  const response = await fetch(`${process.env.ROOT_LINK}/api/signin`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    cache: "no-store",
-    body: JSON.stringify(formData),
-  });
-  if (!response.ok) {
-    throw Error("Invalid credentials");
-  }
-  const res = await response.json();
-  return await res.user;
-};
