7da9bc53b61e3a84611690d117c49265cd296730
enonjovu <enochnjovuyalema20@gmail.com>
2024-05-10 11:47:05 +0200

    added prettier formatting

---
+++ app/(admin)/components/TextEditor.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import {useEditor, EditorContent} from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
+type TextEditorProps = {
+	onChange?: () => void;
+	content?: string;
+};
+
+export default function TextEditor({onChange, content}: TextEditorProps) {
+	const editor = useEditor({
+		extensions: [StarterKit],
+		content: '',
+	});
+
+	return <EditorContent editor={editor} />;
+}
