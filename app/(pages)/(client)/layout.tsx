91d4e2424a305be17146649c9bcb1f2ffb95209f
enonjovu <enochnjovuyalema20@gmail.com>
2024-06-01 07:27:22 +0200

    wip

--- app/(pages)/(client)/layout.tsx
+++ app/(pages)/(client)/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     </InterFontWrapper>
                     <PrelineScript />
                 </AuthProvider>
-                <InjectAnalyticsScripts />
             </body>
         </html>
     );
